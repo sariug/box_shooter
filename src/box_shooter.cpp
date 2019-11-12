acf7ff83f6a0bdafc9a5f57c5b7fbdab1345a0b8
sariug <ugurcansari93@gmail.com>
2019-11-12 14:12:35 +0100

    main script clean up

--- src/box_shooter.cpp
+++ src/box_shooter.cpp
@@ -3,7 +3,6 @@
 
 #include "Window.h"
 #include "Camera.h"
-
 #include "btBulletDynamicsCommon.h"
 
 GLfloat deltaTime = .0f;
@@ -13,34 +12,145 @@ Window *mainwindow = new Window;
 Camera *camera;
 std::vector<Box *> shapes;
 /*  Bullet World*/
-
-///collision configuration contains default setup for memory, collision setup. Advanced users can create their own configuration.
-btDefaultCollisionConfiguration *collisionConfiguration = new btDefaultCollisionConfiguration();
-
-///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
-btCollisionDispatcher *dispatcher = new btCollisionDispatcher(collisionConfiguration);
-
-///btDbvtBroadphase is a good general purpose broadphase. You can also try out btAxis3Sweep.
-btBroadphaseInterface *overlappingPairCache = new btDbvtBroadphase();
-
-///the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
-btSequentialImpulseConstraintSolver *solver = new btSequentialImpulseConstraintSolver;
-
-btDiscreteDynamicsWorld *dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
-//keep track of the shapes, we release memory at exit.
+btDefaultCollisionConfiguration *collisionConfiguration;
+btDiscreteDynamicsWorld *dynamicsWorld;
+btBroadphaseInterface *overlappingPairCache;
+btSequentialImpulseConstraintSolver *solver;
+btCollisionDispatcher *dispatcher;
 //make sure to re-use collision shapes among rigid bodies whenever possible!
 btAlignedObjectArray<btCollisionShape *> collisionShapes;
 void check_collisions();
 void process_keys();
+void init_physics();
+void init_visual_shapes();
+void init_collision_shapes();
+void delete_physics();
 int main()
 {
-	mainwindow->initialize();
-	dynamicsWorld->setGravity(btVector3(0, -10, 0));
 
+	mainwindow->initialize();
+
+	init_physics();
+	init_visual_shapes();
+	init_collision_shapes();
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	camera = new Camera(glm::vec3(-20.0f, 20.0f, -20.0f), glm::vec3(.0f, 1.0f, .0f), 0.0f, -30.0f, 5.0f, .5f);
+
+	glm::mat4 projection = glm::perspective(45.0f, mainwindow->getBufferWidth() / mainwindow->getBufferHeight(), 0.01f, 100.0f);
+	// Loop until it is closed
+	while (!mainwindow->shouldWindowClose())
+	{
+		check_collisions();
+
+		GLfloat now = glfwGetTime();
+		deltaTime = now - lastTime;
+		lastTime = now;
+		// Get and handle user inputs
+		glfwPollEvents();
+		process_keys();
+		camera->mouseControl(glm::vec2(mainwindow->getXchange(), mainwindow->getYchange()));
+		// Clear window
+		glClearColor(0.0f, 0.0f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		for (auto &s : shapes)
+			s->draw(projection, camera->calculateViewMatrix());
+
+		mainwindow->swapBuffers();
+	}
+	// clean
+	delete_physics();
+	return 0;
+}
+void check_collisions()
+{
+	dynamicsWorld->stepSimulation(deltaTime, 10);
+
+	//print positions of all objects
+	for (int j = dynamicsWorld->getNumCollisionObjects() - 1; j >= 0; j--)
+	{
+		btCollisionObject *obj = dynamicsWorld->getCollisionObjectArray()[j];
+		btRigidBody *body = btRigidBody::upcast(obj);
+		btTransform trans;
+		if (body && body->getMotionState())
+		{
+			body->getMotionState()->getWorldTransform(trans);
+		}
+		else
+		{
+			trans = obj->getWorldTransform();
+		}
+		trans.getOpenGLMatrix(glm::value_ptr(shapes.at(j)->set_transformation()));
+		printf("world pos object %d = %f,%f,%f\n", j, float(trans.getOrigin().getX()), float(trans.getOrigin().getY()), float(trans.getOrigin().getZ()));
+		if (float(trans.getOrigin().getY()) > -100.0)
+			continue;
+
+		// Delete from bullet
+		if (body && body->getMotionState())
+		{
+			delete body->getMotionState();
+		}
+		dynamicsWorld->removeCollisionObject(obj);
+		delete obj;
+		btCollisionShape *shape = collisionShapes[j];
+		collisionShapes[j] = 0;
+		//delete shape;
+		// Delete from my list
+		shapes.erase(std::begin(shapes) + j);
+	}
+}
+
+void process_keys()
+{
+	auto keys = mainwindow->getKeys();
+	if (keys[GLFW_KEY_SPACE])
+	{
+		shapes.push_back(new Box(-20.0f, 20.0f, -20.0f));
+		auto origin = shapes.back()->getOrigin();
+		btCollisionShape *colShape = new btBoxShape(btVector3(1, 1, 1));
+		collisionShapes.push_back(colShape);
+
+		/// Create Dynamic Objects
+		btTransform startTransform;
+		startTransform.setIdentity();
+
+		btScalar mass(1.f);
+
+		//rigidbody is dynamic if and only if mass is non zero, otherwise static
+		bool isDynamic = (mass != 0.f);
+
+		btVector3 localInertia(0, 0, 0);
+		if (isDynamic)
+			colShape->calculateLocalInertia(mass, localInertia);
+
+		startTransform.setOrigin(btVector3(origin.x, origin.y, origin.z));
+
+		//using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+		btDefaultMotionState *myMotionState = new btDefaultMotionState(startTransform);
+		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, colShape, localInertia);
+		btRigidBody *body = new btRigidBody(rbInfo);
+		btVector3 direction;
+		direction.setX(camera->get_front().x);
+		direction.setY(camera->get_front().y);
+		direction.setZ(camera->get_front().z);
+
+		body->setLinearVelocity(direction * 25);
+		dynamicsWorld->addRigidBody(body);
+	}
+}
+
+void init_visual_shapes()
+{
+	// Items
+	// Ground
 	Box *ground = new Box(2, -2, 2);
 	ground->scaleBox(glm::vec3(10.0f, 1.0f, 10.0f));
-	shapes.push_back(ground);
 
+	shapes.push_back(ground);
+	// Other
 	for (int i = 0; i < 5; i++)
 	{
 		for (int j = 0; j < 5; j++)
@@ -51,9 +161,9 @@ int main()
 			}
 		}
 	}
-
-	///-----initialization_end-----
-
+}
+void init_collision_shapes()
+{
 	///create a few basic rigid bodies
 	{
 		btCollisionShape *groundShape = new btBoxShape(btVector3(btScalar(10.), btScalar(1.), btScalar(10.)));
@@ -62,7 +172,7 @@ int main()
 
 		btTransform groundTransform;
 		groundTransform.setIdentity();
-		auto origin = ground->getOrigin();
+		auto origin = shapes[0]->getOrigin();
 		groundTransform.setOrigin(btVector3(origin.x, origin.y, origin.z));
 
 		btScalar mass(0.);
@@ -117,35 +227,18 @@ int main()
 			dynamicsWorld->addRigidBody(body);
 		}
 	}
-	//glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+}
 
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
-	glCullFace(GL_BACK);
-	camera = new Camera(glm::vec3(-20.0f, 20.0f, -20.0f), glm::vec3(.0f, 1.0f, .0f), 0.0f, -30.0f, 5.0f, .5f);
-
-	glm::mat4 projection = glm::perspective(45.0f, mainwindow->getBufferWidth() / mainwindow->getBufferHeight(), 0.01f, 100.0f);
-	// Loop until it is closed
-	while (!mainwindow->shouldWindowClose())
-	{
-		check_collisions();
-
-		GLfloat now = glfwGetTime();
-		deltaTime = now - lastTime;
-		lastTime = now;
-		// Get and handle user inputs
-		glfwPollEvents();
-		process_keys();
-		camera->mouseControl(glm::vec2(mainwindow->getXchange(), mainwindow->getYchange()));
-		// Clear window
-		glClearColor(0.0f, 0.0f, 0.1f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		for (auto &s : shapes)
-			s->draw(projection, camera->calculateViewMatrix());
-
-		mainwindow->swapBuffers();
-	}
+void init_physics()
+{
+	collisionConfiguration = new btDefaultCollisionConfiguration();
+	dispatcher = new btCollisionDispatcher(collisionConfiguration);
+	overlappingPairCache = new btDbvtBroadphase();
+	solver = new btSequentialImpulseConstraintSolver;
+	dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+}
+void delete_physics()
+{
 	//delete dynamics world
 	delete dynamicsWorld;
 
@@ -162,81 +255,4 @@ int main()
 
 	//next line is optional: it will be cleared by the destructor when the array goes out of scope
 	collisionShapes.clear();
-	return 0;
-}
-void check_collisions()
-{
-	dynamicsWorld->stepSimulation(deltaTime, 10);
-
-	//print positions of all objects
-	for (int j = dynamicsWorld->getNumCollisionObjects() - 1; j >= 0; j--)
-	{
-		btCollisionObject *obj = dynamicsWorld->getCollisionObjectArray()[j];
-		btRigidBody *body = btRigidBody::upcast(obj);
-		btTransform trans;
-		if (body && body->getMotionState())
-		{
-			body->getMotionState()->getWorldTransform(trans);
-		}
-		else
-		{
-			trans = obj->getWorldTransform();
-		}
-		trans.getOpenGLMatrix(glm::value_ptr(shapes.at(j)->set_transformation()));
-		//printf("world pos object %d = %f,%f,%f\n", j, float(trans.getOrigin().getX()), float(trans.getOrigin().getY()), float(trans.getOrigin().getZ()));
-		if (float(trans.getOrigin().getY()) > -100.0)
-			continue;
-
-		// Delete from bullet
-		if (body && body->getMotionState())
-		{
-			delete body->getMotionState();
-		}
-		dynamicsWorld->removeCollisionObject(obj);
-		delete obj;
-		btCollisionShape *shape = collisionShapes[j];
-		collisionShapes[j] = 0;
-		delete shape;
-		// Delete from my list
-		shapes.erase(std::begin(shapes) + j);
-	}
-}
-
-void process_keys()
-{
-	auto keys = mainwindow->getKeys();
-	if (keys[GLFW_KEY_SPACE])
-	{
-		shapes.push_back(new Box(-20.0f, 20.0f, -20.0f));
-		auto origin = shapes.back()->getOrigin();
-		btCollisionShape *colShape = new btBoxShape(btVector3(1, 1, 1));
-		collisionShapes.push_back(colShape);
-
-		/// Create Dynamic Objects
-		btTransform startTransform;
-		startTransform.setIdentity();
-
-		btScalar mass(1.f);
-
-		//rigidbody is dynamic if and only if mass is non zero, otherwise static
-		bool isDynamic = (mass != 0.f);
-
-		btVector3 localInertia(0, 0, 0);
-		if (isDynamic)
-			colShape->calculateLocalInertia(mass, localInertia);
-
-		startTransform.setOrigin(btVector3(origin.x, origin.y, origin.z));
-
-		//using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
-		btDefaultMotionState *myMotionState = new btDefaultMotionState(startTransform);
-		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, colShape, localInertia);
-		btRigidBody *body = new btRigidBody(rbInfo);
-		btVector3 direction;
-		direction.setX(camera->get_front().x);
-		direction.setY(camera->get_front().y);
-		direction.setZ(camera->get_front().z);
-
-		body->setLinearVelocity(direction * 25);
-		dynamicsWorld->addRigidBody(body);
-	}
 }
